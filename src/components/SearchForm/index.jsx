a976e5356f887211f6f35aac3d190164d44cb1a4
asiek728 <asiek728@gmail.com>
2023-12-11 14:41:17 +0000

    add SearchForm component

---
+++ src/components/SearchForm/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const SearchForm = ({ setShowData }) => {
+    const [inputValue, setInputValue] = useState("")
+    const [searchString, setSearchString] = useState("")
+
+    useEffect(() => {
+        const searchAPI = async () => {
+            const { data } = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchString}`)
+            const shows = data.map(s => s.show)
+            setShowData(shows)
+        }
+        searchAPI()
+    }, [searchString])
+
+
+    const handleInput = (e) => {
+        const newInput = e.target.value
+        setInputValue(newInput)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setSearchString(inputValue)
+        setInputValue("")
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={inputValue} onChange={handleInput} required />
+                <input type="submit" value="Search" />
+            </form>
+        </>
+    )
+}
+
+export default SearchForm
--- src/components/SearchWidget/index.jsx
+++ src/components/SearchWidget/index.jsx
@@ -1,42 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { ShowList } from "../"
+import React, { useState } from 'react'
+import { ShowList, SearchForm } from "../"
 
 const SearchWidget = () => {
 
-    const [inputValue, setInputValue] = useState("")
-    const [searchString, setSearchString] = useState("")
     const [showData, setShowData] = useState([])
-
-    const handleInput = (e) => {
-        const newInput = e.target.value
-        setInputValue(newInput)
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setSearchString(inputValue)
-        setInputValue("")
-    }
-
-    useEffect(() => {
-        const searchAPI = async () => {
-            const { data } = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchString}`)
-            const shows = data.map(s => s.show)
-            setShowData(shows)
-        }
-        searchAPI()
-    }, [searchString])
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={inputValue} onChange={handleInput} required />
-                <input type="submit" value="Search" />
-            </form>
+            <SearchForm setShowData={setShowData}/>
             <ShowList showData={showData}/>
         </>
-
     )
 }
 
